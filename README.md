4f37d8bb8369a746f930995b08aebd4d4a51b13c
Andy Adiwidjaja <adiwidjaja@gmail.com>
2018-09-12 10:19:38 +0200

    Element type filtering, readme.

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # silverstripe-elemental-base
 Silverstripe Elemental Alternate Base
+
+# Usage
+
+elements.yml:
+
+    ---
+    Name: myelements
+    After: silverstripe-elemental-textimage
+    ---
+    Page:
+      extensions:
+      - ATW\ElementalBase\Extensions\ElementalPageExtension
+      ATW\ElementalTextImage\Models\ElementTextImage:
+        variants:
+          section--imageleft: 'Image Left'
+          section--imageright: 'Image Right'
+        variants_name: 'Image direction'
+      ATW\ElementalBase\Models\ElementText:
+        options:
+          section--highlight: 'Highlight'
+        options_name: 'Options'
+    ATW\ElementalBase\Models\BaseElement:
+      use_submenu: false
+
+# Options and Variants
+
+Variants and options can be defined in the config. 
+They are displayed in the CMS as dropdown (Variants)
+and checkboxes (Options).
+
+They can be included in the template. 
+
+ElementTextImage.ss:
+
+    <section class="section section--textimage $VariantClasses" id="$Anchor">
+        <div class="section_content">
+            <% if $ShowTitle %>
+                <h2>$Title</h2>
+            <% end_if %>
+            <div class="section_items">
+                <div class="text">
+                    $Text
+                </div>
+    
+                <div class="image">
+                    $Image
+                </div>
+    
+            </div>
+        </div>
+    </section>
+
+# Submenu
+
+For the generation of anchor menus:
+
+    ATW\ElementalBase\Models\BaseElement:
+      use_submenu: true
+
+Displays title and option for the submenu in the CMS.
+
+In the template:
+
+    <% if $SubMenu %>
+    <ul class="submenu">
+        <% loop $SubMenu %>
+        <li><a class="menu--section" href="$Link" data-scroll="$Link">$Title</a></li>
+        <% end_loop %>
+    </ul>
+    <% end_if %>
+
+
+# Menu Extension
+
+Not really related:
+
+    Page:
+      extensions:
+      - ATW\ElementalBase\Extensions\MenuPageExtension
+      menus:
+        main: "Main menu"
+        footer: "Footer menu"
+    PageController:
+      extensions:
+      - ATW\ElementalBase\Extensions\MenuContentControllerExtension
+
+In the template:
+
+    <% loop $FilteredMenu("main", 1) %>
+    <li><a class="menu--$LinkOrSection" href="$Link">$MenuTitle</a></li>
+    <% end_loop %>
--- src/Extensions/ElementalPageExtension.php
+++ src/Extensions/ElementalPageExtension.php
@@ -1,8 +1,11 @@
 <?php
 namespace ATW\ElementalBase\Extensions;
 
+use ATW\ElementalBase\Models\BaseElement;
+use DNADesign\Elemental\Models\BaseElement as DNABase;
 use DNADesign\Elemental\Extensions\ElementalPageExtension as BaseExtension;
 use DNADesign\Elemental\Models\ElementalArea;
+use SilverStripe\Core\ClassInfo;
 use SilverStripe\ORM\ArrayList;
 use SilverStripe\View\ArrayData;
 
@@ -20,6 +23,62 @@ class ElementalPageExtension extends BaseExtension {
         }
 
         return $menu;
+    }
+
+    /**
+     * Get the available element types for this page type,
+     *
+     * Uses allowed_elements, stop_element_inheritance, disallowed_elements in
+     * order to get to correct list.
+     *
+     * Only subclasses of our subclass
+     *
+     * @return array
+     */
+    public function getElementalTypes()
+    {
+        $config = $this->owner->config();
+
+        if (is_array($config->get('allowed_elements'))) {
+            if ($config->get('stop_element_inheritance')) {
+                $availableClasses = $config->get('allowed_elements', Config::UNINHERITED);
+            } else {
+                $availableClasses = $config->get('allowed_elements');
+            }
+        } else {
+            $availableClasses = ClassInfo::subclassesFor(BaseElement::class);
+        }
+
+        $disallowedElements = (array) $config->get('disallowed_elements');
+        $list = array();
+
+        foreach ($availableClasses as $availableClass) {
+            /** @var BaseElement $inst */
+            $inst = singleton($availableClass);
+
+            if (!in_array($availableClass, $disallowedElements) && $inst->canCreate()) {
+                if ($inst->hasMethod('canCreateElement') && !$inst->canCreateElement()) {
+                    continue;
+                }
+
+                $list[$availableClass] = $inst->getType();
+            }
+        }
+
+        if ($config->get('sort_types_alphabetically') !== false) {
+            asort($list);
+        }
+
+        if (isset($list[DNABase::class])) {
+            unset($list[DNABase::class]);
+        }
+        if (isset($list[BaseElement::class])) {
+            unset($list[BaseElement::class]);
+        }
+
+        $this->owner->invokeWithExtensions('updateAvailableTypesForClass', $class, $list);
+
+        return $list;
     }
 
     public function getElementsForMenu()
