13397ff2364dc6b9dcfc253355943334f866e360
Andy Adiwidjaja <adiwidjaja@gmail.com>
2019-11-29 10:07:41 +0100

    Fluent extension.

---
+++ src/Extensions/ElementalPageFluentExtension.php
@@ -0,0 +1,24 @@
+<?php
+namespace ATW\ElementalBase\Extensions;
+
+use SilverStripe\Core\Extension;
+use TractorCow\Fluent\Model\Locale;
+
+class ElementalPageFluentExtension extends Extension
+{
+    /**
+     * Override default Fluent fallback
+     *
+     * @param string $query
+     * @param string $table
+     * @param string $field
+     * @param Locale $locale
+     */
+    public function updateLocaliseSelect(&$query, $table, $field, Locale $locale)
+    {
+        // disallow elemental data inheritance in the case that published localised page instance already exists
+        if ($field == 'ElementalAreaID' && $this->owner->isPublishedInLocale()) {
+            $query = '"' . $table . '_Localised_' . $locale->getLocale() . '"."' . $field . '"';
+        }
+    }
+}
